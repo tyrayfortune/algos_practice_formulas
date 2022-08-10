f0cfbb52f1339c23cc73e7f88639c658f3092f82
tyrayfortune <99374142+tyrayfortune@users.noreply.github.com>
2022-08-10 12:16:32 -0700

    algos

---
+++ formulas/data structures/SPREAD_combine_arrays.js
@@ -0,0 +1,10 @@
+// With more traditional syntaxes, we can concatenate arrays, but this only allows us to combine arrays at the end of one, 
+// and at the start of another. Spread syntax makes the following operation extremely simple:
+
+let thisArray = ['sage', 'rosemary', 'parsley', 'thyme'];
+
+let thatArray = ['basil', 'cilantro', ...thisArray, 'coriander'];
+// thatArray would have the value ['basil', 'cilantro', 'sage', 'rosemary', 'parsley', 'thyme', 'coriander'].
+
+// Using spread syntax, we have just achieved an operation that would have been more complex and more verbose had we used traditional 
+// methods.
