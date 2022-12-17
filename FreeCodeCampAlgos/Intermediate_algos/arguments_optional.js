4e93ceddfb68bb3421b7ea73e8148c58b7cbc1d5
tyrayfortune <99374142+tyrayfortune@users.noreply.github.com>
2022-12-16 16:48:29 -0800

    algos

--- FreeCodeCampAlgos/Intermediate_algos/arguments_optional.js
+++ FreeCodeCampAlgos/Intermediate_algos/arguments_optional.js
@@ -8,3 +8,76 @@
 
 // If either argument isn't a valid number, return undefined.
 
+//expexcted cases 
+// addTogether(2, 3) should return 5.
+// addTogether(23, 30) should return 53.
+// addTogether(5)(7) should return 12.
+// addTogether("https://www.youtube.com/watch?v=dQw4w9WgXcQ") should return undefined.
+// addTogether(2, "3") should return undefined.
+// addTogether(2)([3]) should return undefined.
+// addTogether("2", 3) should return undefined.
+// addTogether(5, undefined) should return undefined.
+
+//given function
+// function addTogether() {
+//     return false;
+//   }
+
+
+function addTogether(x,y) {
+    if (typeof(x) !== "number"){
+        return undefined
+    }
+    else if (y) {
+        if (typeof(y) !=="number") {
+            return undefined
+        }
+    }
+    else if(!y){
+        function addSecond(y){
+            if (typeof(y) !== "number") {
+                return undefined
+            }
+            else{
+                return addSecond
+            }
+        }
+    }
+    return x + y
+}
+    function addTogether() {
+        const [first, second] = arguments;
+        // First argument is not a number
+        if (typeof(first) !== "number") {
+        return undefined;
+        }
+        // First argument is a number
+        //  and second argument is not defined
+        else if (arguments.length === 1) {
+        function addSecond(newNumber) {
+            // New argument is not a number
+            if (typeof(newNumber) !== "number") {
+            return undefined;
+            }
+            // New argument is a number
+            else {
+            return first + newNumber
+            }
+        }
+        // Note: returning a *function*
+        return addSecond;
+        }
+        // First argument is a number
+        //  and second argument is not a number
+        else if (typeof(second) !== "number") {
+        return undefined;
+        }
+        // First argument is a number
+        //  and second argument is a number
+        else {
+        return first + second;
+        }
+    }
+  console.log(addTogether(2,3));
+//   console.log(addTogether(3));
+console.log(addTogether(5)(7))
