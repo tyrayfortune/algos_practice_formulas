e9d3e44cee1f52b04748d8702070a77afff46a26
tyrayfortune <99374142+tyrayfortune@users.noreply.github.com>
2023-01-08 12:34:00 -0800

    algos

---
+++ practice/new.js
@@ -0,0 +1,26 @@
+function wordFrequency(string) {
+    // Create an empty object to store the frequency of each word
+    const frequency = {};
+  
+    // Split the string into an array of words
+    const words = string.split(' ');
+  
+    // Loop through the array of words
+    for (const word of words) {
+      // If the word is not in the frequency object, set its frequency to 1
+      if (!frequency[word]) {
+        frequency[word] = 1;
+      } else {
+        // If the word is already in the frequency object, increment its frequency by 1
+        frequency[word] += 1;
+      }
+    }
+  
+    // Return the frequency object
+    return frequency;
+  }
+  
+  // Test the function
+  console.log(wordFrequency('the quick brown fox jumps over the lazy dog'));
+  // Output: { the: 2, quick: 1, brown: 1, fox: 1, jumps: 1, over: 1, lazy: 1, dog: 1 }
+  
