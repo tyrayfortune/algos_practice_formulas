dab85c27d3b3b6c37381bcb8484814dc71de118a
tyrayfortune <99374142+tyrayfortune@users.noreply.github.com>
2023-01-31 20:37:40 -0800

    algos

---
+++ leetcode/easy/ransom_note.js
@@ -0,0 +1,32 @@
+// Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters
+//  from magazine and false otherwise.
+// Each letter in magazine can only be used once in ransomNote.
+
+// Example 1:
+// Input: ransomNote = "a", magazine = "b"
+// Output: false
+
+// Example 2:
+// Input: ransomNote = "aa", magazine = "ab"
+// Output: false
+
+// Example 3:
+// Input: ransomNote = "aa", magazine = "aab"
+// Output: true
+
+//given function
+// var canConstruct = function(ransomNote, magazine) {
+    
+// };
+
+var canConstruct = function(ransomNote, magazine) {
+    return 
+};
+
+console.log(canConstruct("a","b"))
+console.log(canConstruct("aa","aab"))
+
+//logic:
+//take the string and put each letter into a array using split
+//for loop using the includes method, and if so popping/spicing current element
+//return true/false boolean
