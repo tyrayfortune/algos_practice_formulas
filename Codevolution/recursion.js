0113a1ca521a9e4469c4ad77c830bdf882c091c5
tyrayfortune <99374142+tyrayfortune@users.noreply.github.com>
2023-03-09 14:00:00 -0800

    algo

---
+++ Codevolution/recursion.js
@@ -0,0 +1,10 @@
+//what? - recursion is a problem solving technique where the solution depnds on solutioons to smaller 
+//instances of the same problem, i.e where a function calls itself
+
+// why? - a great technique to simplify your solution
+
+// points - 
+        // every recursive solution needs a base case to terminate recursion
+        // just because it may simplify answer, does not mean its faster
+        // recursion is not the straightforward to understand, its difficult.
+
