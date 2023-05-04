1d0ae7279e8b43170483ddf68f043759e1393080
tyrayfortune <99374142+tyrayfortune@users.noreply.github.com>
2023-05-04 13:12:07 -0700

    algos

---
+++ leetcode/easy/counter.js
@@ -0,0 +1,39 @@
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 
+// more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+// Example 1:
+// Input: 
+// n = 10 
+// ["call","call","call"]
+// Output: [10,11,12]
+// Explanation: 
+// counter() = 10 // The first time counter() is called, it returns n.
+// counter() = 11 // Returns 1 more than the previous time.
+// counter() = 12 // Returns 1 more than the previous time.
+
+// // Example 2:
+// // Input: 
+// n = -2
+// ["call","call","call","call","call"]
+// Output: [-2,-1,0,1,2]
+// Explanation: counter() initially returns -2. Then increases after each sebsequent call.
+
+// given function
+// var createCounter = function(n) {
+//     return function() {
+        
+//     };
+// };
+
+
+var createCounter = function(n) {
+    //returning an arrow function of n + 1, so every time it gets added
+    return () => n++
+};
+
+const counter = createCounter(10)
+console.log(counter()) // 10
+console.log(counter()) // 11
+console.log(counter()) // 12
+console.log(counter()) // 13
+
